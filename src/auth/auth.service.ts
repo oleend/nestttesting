a9fef21d5bd1f4e4fd94fb61c910c2de5da3c8bd
oleend <davidpoleen@gmail.com>
2023-08-26 04:25:29 -0400

    init

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@nestjs/common";
+
+
+@Injectable({})
+export class AuthService {
+    login() {
+        return {msg : "I have signed up"};
+    }
+    signup() {
+        return { msg : "I have signed in"}
+    }
+}
+
+
+const service = new AuthService()
